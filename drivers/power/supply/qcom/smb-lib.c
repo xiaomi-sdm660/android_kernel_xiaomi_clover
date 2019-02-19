52bf2ea0751e8657b9bd8a67490a17409b8efe0b
Harsh Shandilya <msfjarvis@gmail.com>
2019-02-19 17:40:06 +0530

    power: smb-lib: Silence spammy debugging
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -31,6 +31,7 @@
 #include <linux/fastchg.h>
 #endif
 
+#ifdef DEBUG
 #define smblib_err(chg, fmt, ...)		\
 	pr_err("%s: %s: " fmt, chg->name,	\
 		__func__, ##__VA_ARGS__)	\
@@ -44,6 +45,10 @@
 			pr_debug("%s: %s: " fmt, chg->name,	\
 				__func__, ##__VA_ARGS__);	\
 	} while (0)
+#else
+#define smblib_err(chg, fmt, ...) do {} while (0)
+#define smblib_dbg(chg, reason, fmt, ...) do {} while (0)
+#endif
 
 
 extern struct smb_charger *smbchg_dev;
@@ -3296,6 +3301,7 @@ int smblib_get_prop_slave_current_now(struct smb_charger *chg,
  * INTERRUPT HANDLERS *
  **********************/
 
+#ifdef DEBUG
 irqreturn_t smblib_handle_debug(int irq, void *data)
 {
 	struct smb_irq_data *irq_data = data;
@@ -3303,6 +3309,12 @@ irqreturn_t smblib_handle_debug(int irq, void *data)
 
 	return IRQ_HANDLED;
 }
+#else
+inline irqreturn_t smblib_handle_debug(__attribute__((unused)) int irq, __attribute__((unused)) void *data)
+{
+	return IRQ_HANDLED;
+}
+#endif
 
 irqreturn_t smblib_handle_otg_overcurrent(int irq, void *data)
 {
