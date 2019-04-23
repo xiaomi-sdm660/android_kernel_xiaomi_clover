0aea4da379a4b9c6e234567af199c59b96e5990e
Max Weffers <rcstar6696@gmail.com>
2019-04-23 16:36:48 +0200

    sound:tas2557: remove irq status debug

--- sound/soc/codecs/tas2557/tas2557-regmap.c
+++ sound/soc/codecs/tas2557/tas2557-regmap.c
@@ -779,8 +779,6 @@ static void irq_work_routine(struct work_struct *work)
 
 			goto program;
 		} else {
-			dev_dbg(pTAS2557->dev, "IRQ status L: 0x%x, 0x%x\n",
-				nDevLInt1Status, nDevLInt2Status);
 			nCounter = 2;
 			while (nCounter > 0) {
 				nResult = tas2557_dev_read(pTAS2557, channel_left, TAS2557_POWER_UP_FLAG_REG, &nDevLInt1Status);
@@ -870,8 +868,6 @@ static void irq_work_routine(struct work_struct *work)
 				pTAS2557->mnErrCode &= ~ERROR_CLK_DET2;
 			goto program;
 		} else {
-			dev_dbg(pTAS2557->dev, "IRQ status R: 0x%x, 0x%x\n",
-				nDevRInt1Status, nDevRInt2Status);
 			nCounter = 2;
 			while (nCounter > 0) {
 				nResult = tas2557_dev_read(pTAS2557, channel_right, TAS2557_POWER_UP_FLAG_REG, &nDevRInt1Status);
