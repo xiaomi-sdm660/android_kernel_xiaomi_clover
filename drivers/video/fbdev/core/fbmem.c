df00fc15f12d9ec989a997cda4af8368b9a84bb5
1582130940 <1582130940@qq.com>
2019-08-21 18:31:09 +0800

    drivers/video: Import Xiaomi changes

--- drivers/video/fbdev/core/fbmem.c
+++ drivers/video/fbdev/core/fbmem.c
@@ -1061,12 +1061,20 @@ EXPORT_SYMBOL(fb_set_var);
 
 int
 fb_blank(struct fb_info *info, int blank)
-{	
+{
 	struct fb_event event;
 	int ret = -EINVAL, early_ret;
 
  	if (blank > FB_BLANK_POWERDOWN)
  		blank = FB_BLANK_POWERDOWN;
+
+#ifdef CONFIG_XIAOMI_CLOVER
+	if (info->blank == blank) {
+		if (info->fbops->fb_blank)
+			ret = info->fbops->fb_blank(blank, info);
+		return ret;
+	}
+#endif
 
 	event.info = info;
 	event.data = &blank;
@@ -1086,6 +1094,11 @@ fb_blank(struct fb_info *info, int blank)
 		if (!early_ret)
 			fb_notifier_call_chain(FB_R_EARLY_EVENT_BLANK, &event);
 	}
+
+#ifdef CONFIG_XIAOMI_CLOVER
+	if (!ret)
+		info->blank = blank;
+#endif
 
  	return ret;
 }
@@ -1648,6 +1661,9 @@ static int do_register_framebuffer(struct fb_info *fb_info)
 		if (!registered_fb[i])
 			break;
 	fb_info->node = i;
+#ifdef CONFIG_XIAOMI_CLOVER
+	fb_info->blank = -1;
+#endif
 	atomic_set(&fb_info->count, 1);
 	mutex_init(&fb_info->lock);
 	mutex_init(&fb_info->mm_lock);
